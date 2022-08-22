a474bfd7f493fdfc5f32e7a63ec0db39222d01d1
theendercore <theendercore@gmail.com>
2022-08-22 12:46:20 +0300

    done code

---
+++ src/main/java/com/theendercore/visibletogglesprint/VisibleToggleSprint.java
@@ -0,0 +1,15 @@
+package com.theendercore.visibletogglesprint;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VisibleToggleSprint implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("visible_toggle_sprint");
+
+	@Override
+	public void onInitialize() {
+	LOGGER.info("I am my planting roots.");
+
+	}
+}
---
+++ src/main/java/com/theendercore/visibletogglesprint/mixin/CrosshairMixin.java
@@ -0,0 +1,35 @@
+package com.theendercore.visibletogglesprint.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public class CrosshairMixin {
+    @Final
+    @Shadow
+    private MinecraftClient client;
+    private boolean clean = false;
+
+    @Inject(method = "renderCrosshair", at = @At("TAIL"))
+    private void renderCrosshair(MatrixStack matrices, CallbackInfo ci) {
+        GameOptions gameOptions = this.client.options;
+        if (gameOptions.sprintKey.isPressed()) {
+            client.inGameHud.setOverlayMessage(Text.translatable("overlay.visible_toggle_sprint.text.on").formatted(Formatting.ITALIC), false);
+            clean = true;
+        }
+        if (clean && !gameOptions.sprintKey.isPressed()) {
+            client.inGameHud.setOverlayMessage(Text.literal("overlay.visible_toggle_sprint.text.off").formatted(Formatting.RED, Formatting.ITALIC), false);
+            clean = false;
+        }
+    }
+}
