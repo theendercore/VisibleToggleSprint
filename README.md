244a09e3ccb55de8b06329a529c6fd951510ce42
theendercore <theendercore@gmail.com>
2022-08-24 14:15:45 +0300

    i did it | 1.1.0

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Visible Toggle Sprint
-A simple mod to make toggle sprint easier!
+A simple mod to make toggle sprint & sneak easier!
 
 I struggle to remember if I have toggle sprint on or not, so I just decided to amke this mod to make it easier.
-Also, for the future I want to have a little icon appear next to the crosshair and not text above the hotbar. But I don't know how rendering(for now).
-*If you know pls send me a message [Ender#8344]*
-### Dependencies
-This mod Depends on [FabricAPI](https://modrinth.com/mod/fabric-api) or [Quilted Fabric API](https://modrinth.com/mod/qsl)
+The mod also renders toggle sneak. So all life is much essayer if you are dumb like me :)
--- src/main/java/com/theendercore/visibletogglesprint/mixin/CrosshairMixin.java
+++ src/main/java/com/theendercore/visibletogglesprint/mixin/CrosshairMixin.java
@@ -4,23 +4,18 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static com.theendercore.visibletogglesprint.VisibleToggleSprint.MODID;
 
 @Mixin(InGameHud.class)
 public class CrosshairMixin extends DrawableHelper {
-    private final Identifier modIcons = new Identifier(MODID, "textures/icons.png");
+    private final Identifier modIcons = new Identifier(MODID, "textures/gui/icons.png");
     @Final
     @Shadow
     private MinecraftClient client;
@@ -28,22 +23,17 @@ public class CrosshairMixin extends DrawableHelper {
     private int scaledWidth;
     @Shadow
     private int scaledHeight;
-    private boolean clean = false;
 
     @Inject(method = "renderCrosshair", at = @At("TAIL"))
     private void renderCrosshair(MatrixStack matrices, CallbackInfo ci) {
-
         RenderSystem.setShaderTexture(0, modIcons);
-        this.drawTexture(matrices, (this.scaledWidth - 15) / 2 - 15, (this.scaledHeight - 15) / 2, 0, 0, 15, 15);
 
-        GameOptions gameOptions = this.client.options;
-        if (gameOptions.sprintKey.isPressed()) {
-            client.inGameHud.setOverlayMessage(Text.translatable("overlay.visible_toggle_sprint.text.on").formatted(Formatting.ITALIC), false);
-            clean = true;
+        if (this.client.options.sprintKey.isPressed()) {
+            this.drawTexture(matrices, (this.scaledWidth - 3) / 2 - 4, (this.scaledHeight - 3) / 2 - 4, 0, 0, 4, 4);
         }
-        if (clean && !gameOptions.sprintKey.isPressed()) {
-            client.inGameHud.setOverlayMessage(Text.translatable("overlay.visible_toggle_sprint.text.off").formatted(Formatting.RED, Formatting.ITALIC), false);
-            clean = false;
+        if (this.client.options.sneakKey.isPressed()) {
+            this.drawTexture(matrices, (this.scaledWidth - 3) / 2 + 3, (this.scaledHeight - 3) / 2 + 3, 4, 0, 4, 4);
         }
+        RenderSystem.setShaderTexture(0, Screen.GUI_ICONS_TEXTURE);
     }
 }
