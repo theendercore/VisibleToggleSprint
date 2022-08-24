3624658781d62e97033d2f314342b9a895274d7c
theendercore <theendercore@gmail.com>
2022-08-24 13:20:15 +0300

    rendering attempt

--- src/main/java/com/theendercore/visibletogglesprint/VisibleToggleSprint.java
+++ src/main/java/com/theendercore/visibletogglesprint/VisibleToggleSprint.java
@@ -5,7 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VisibleToggleSprint implements ClientModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("visible_toggle_sprint");
+	public static final String MODID = "visible_toggle_sprint";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitializeClient() {
--- src/main/java/com/theendercore/visibletogglesprint/mixin/CrosshairMixin.java
+++ src/main/java/com/theendercore/visibletogglesprint/mixin/CrosshairMixin.java
@@ -1,11 +1,14 @@
 package com.theendercore.visibletogglesprint.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,15 +16,26 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import static com.theendercore.visibletogglesprint.VisibleToggleSprint.MODID;
+
 @Mixin(InGameHud.class)
-public class CrosshairMixin {
+public class CrosshairMixin extends DrawableHelper {
+    private final Identifier modIcons = new Identifier(MODID, "textures/icons.png");
     @Final
     @Shadow
     private MinecraftClient client;
+    @Shadow
+    private int scaledWidth;
+    @Shadow
+    private int scaledHeight;
     private boolean clean = false;
 
     @Inject(method = "renderCrosshair", at = @At("TAIL"))
     private void renderCrosshair(MatrixStack matrices, CallbackInfo ci) {
+
+        RenderSystem.setShaderTexture(0, modIcons);
+        this.drawTexture(matrices, (this.scaledWidth - 15) / 2 - 15, (this.scaledHeight - 15) / 2, 0, 0, 15, 15);
+
         GameOptions gameOptions = this.client.options;
         if (gameOptions.sprintKey.isPressed()) {
             client.inGameHud.setOverlayMessage(Text.translatable("overlay.visible_toggle_sprint.text.on").formatted(Formatting.ITALIC), false);
